2a044103c5b397fc9829aa5db796e1bd70823304
Fabiana <fa.campos.sp@hotmail.com>
2025-02-04 16:32:19 -0300

    add: cartService

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,17 +1,23 @@
-import { Component, signal } from '@angular/core';
+import { Component, inject, signal } from '@angular/core';
 import { PrimaryButtonComponent } from "../primary-button/primary-button.component";
+import { CartService } from '../../services/cart.service';
+import { NgIf } from '@angular/common';
+
 
 @Component({
   selector: 'app-header',
-  imports: [PrimaryButtonComponent],
+  imports: [PrimaryButtonComponent, NgIf],
+  standalone: true,
   template: ` <div class="bg-slate-100 px-4 py-3 shadow-md flex justify-between items-center"> 
     <span class="text-x1"> My Store </span> 
-    <app-primary-button label="Cart" (btnClicked)="showButtonClicked()"/> </div>`,
+    <app-primary-button [label]="'Cart (' + cartService.cart().length +  ' )'" (btnClicked)="showButtonClicked()"/> </div>`,
 
 
   styles: ``,
 })
 export class HeaderComponent {
+
+  cartService = inject(CartService);
 
   showButtonClicked(){
     console.log('clicked');
